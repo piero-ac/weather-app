2e23b69238dbe41f6c0df613961d799abce392de
Piero Coronado <pacoronado01@gmail.com>
2023-01-26 21:09:21 -0500

    Create function for gettingWeather

--- app.js
+++ app.js
@@ -4,9 +4,20 @@ const currentTemp = document.querySelector("#current-temp");
 const weatherName = document.querySelector("#weather-name");
 const highTemp = document.querySelector("#high-temp");
 const lowTemp = document.querySelector("#low-temp");
+const key = config.MY_API_KEY;
+
+let currentCity = 'Perth Amboy';
+let stateCode = 'NJ';
 
 
 citySearchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log('Submitted');
-})
+});
+
+
+function getWeather(city) {
+    let response = undefined;
+
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},nj,us&appid=${key}`)
+}
